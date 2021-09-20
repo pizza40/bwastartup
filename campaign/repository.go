e5649964e87201d5a40df87ba5d140799f154e36
pizza40 <ravi.attar.shahrulsyah@gmail.com>
2021-09-20 15:41:43 +0700

    campaign repository

--- campaign/repository.go
+++ campaign/repository.go
@@ -1,1 +1,42 @@
 package campaign
+
+import "gorm.io/gorm"
+
+//define contract
+type Repository interface {
+	FindAll() ([]Campaign, error)
+	FindByUserID(userID int) ([]Campaign, error)
+}
+
+//connextion to database struct
+type repository struct {
+	db *gorm.DB
+}
+
+//instace for repository
+func NewRepository(db *gorm.DB) *repository{
+	return &repository{db}
+}
+
+func (r *repository) FindAll() ([]Campaign, error){
+	var campaigns []Campaign
+
+	err := r.db.Preload("CampaignImages", "campaign_images.is_primary = 1").Find(&campaigns).Error
+	if err != nil{
+		return campaigns, err
+	}
+
+	return campaigns, nil
+
+}
+
+func (r *repository) FindByUserID(userID int) ([]Campaign, error){
+	var campaigns []Campaign
+
+	err := r.db.Where("user_id = ?", userID).Preload("CampaignImages", "campaign_images.is_primary = 1").Find(&campaigns).Error
+	if err != nil{
+		return campaigns, err
+	}
+
+	return campaigns, nil
+}
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bwastartup/auth"
+	"bwastartup/campaign"
 	"bwastartup/handler"
 	"bwastartup/helper"
 	"bwastartup/user"
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -32,6 +34,22 @@ func main() {
 	}
 
 	userRepository := user.NewRepository(db)
+	campaignRepository := campaign.NewRepository(db)
+
+	campaigns, err := campaignRepository.FindByUserID(4)
+
+	fmt.Println("debug")
+	fmt.Println("debug")
+	fmt.Println("debug")
+	fmt.Println(len(campaigns))
+
+	for _, campaign := range campaigns{
+		fmt.Println(campaign.Name)
+		if len(campaign.CampaignImages) > 0 {
+			fmt.Println(campaign.CampaignImages[0].FileName)
+		}
+	}
+
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
